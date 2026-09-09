ed2794df509f158e8f378a91a64de59c56148228
Kyra Valiquette <6310411@cegepmv.ca>
2026-09-09 18:32:13 -0400

    Fixed Dependency in app.module.tsg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { PlacesModule } from './places/places.module.js';
 import { ReviewsModule } from './reviews/reviews.module.js';
 
 @Module({
   imports: [PlacesModule, ReviewsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
